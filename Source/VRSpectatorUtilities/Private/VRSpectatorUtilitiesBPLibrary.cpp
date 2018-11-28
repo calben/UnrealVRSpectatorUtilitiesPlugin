aa74f6aafad7d8596643e6e5af1db62ea45b682f
Calem Bendell <calem.j.bendell@gmail.com>
2018-11-28 10:07:35 -0500

    plugin initial version

---
+++ Source/VRSpectatorUtilities/Private/VRSpectatorUtilitiesBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "VRSpectatorUtilitiesBPLibrary.h"
+#include "VRSpectatorUtilities.h"
+
+UVRSpectatorUtilitiesBPLibrary::UVRSpectatorUtilitiesBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UVRSpectatorUtilitiesBPLibrary::VRSpectatorUtilitiesSampleFunction(float Param)
+{
+	return -1;
+}
+
